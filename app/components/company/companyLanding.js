4efc58abb12ec5ca099861fb457053139e680b85
xavierarul <xavier.nelson@tcare.ai>
2025-03-19 11:21:12 +0530

    add company

---
+++ app/components/company/companyLanding.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompanyLanding = () => {
+  return (
+    <div>
+      hellow
+    </div>
+  )
+}
+
+export default CompanyLanding
---
+++ app/our-company/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CompanyLanding from '../components/company/companyLanding'
+
+const page = () => {
+  return (
+    <div>
+        <CompanyLanding/>
+      
+    </div>
+  )
+}
+
+export default page
